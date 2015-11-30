8e9055533070297693bca5065e6f6f0d3cbf63ba
Girish Joshi <gjoshi@groupon.com>
2015-11-29 21:34:19 -0800

    More unittests for binary search.

--- arrays/src/test/java/SearchUtilTest.java
+++ arrays/src/test/java/SearchUtilTest.java
@@ -1,4 +1,4 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  *
@@ -7,7 +7,9 @@ public class SearchUtilTest {
 
     @org.junit.Test
     public void testBinarySearch() throws Exception {
+        assertEquals(SearchUtil.binarySearch(new int[]{-5, -1, 9, 11, 13}, 9), 2);
+        assertEquals(SearchUtil.binarySearch(new int[]{0, 2, 3, 5}, 5), 3);
         assertEquals(SearchUtil.binarySearch(new int[]{1, 5, 9, 11}, 5), 1);
-        assertEquals(SearchUtil.binarySearch(new int[]{1, 2}, 5), -1);
+        assertEquals(SearchUtil.binarySearch(new int[]{-2, -1, 0}, 5), -1);
     }
 }
